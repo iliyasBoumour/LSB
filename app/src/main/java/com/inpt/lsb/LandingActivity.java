6adc38e9e313896bb42ff13f786552196b3c2e1b
iliyasBr <iliyasboumour@gmail.com>
2021-01-12 01:42:22 +0100

    auth layouts

--- app/src/main/java/com/inpt/lsb/LandingActivity.java
+++ app/src/main/java/com/inpt/lsb/LandingActivity.java
@@ -2,8 +2,10 @@ package com.inpt.lsb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -43,11 +45,20 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         Intent intent;
+        Pair[] pairs=new Pair[1];
+
         if (v.getId()==R.id.signInButton){
             intent = new Intent(this, SignInActivity.class);
-        }else {
+            pairs[0]=new Pair<View,String>(signIn,"open");
+        } else {
             intent = new Intent(this, SignUpActivity.class);
+            pairs[0]=new Pair<View,String>(signUp,"open");
         }
-        startActivity(intent);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(this,pairs);
+            startActivity(intent,options.toBundle());
+        }else{
+            startActivity(intent);
+        }
     }
 }
